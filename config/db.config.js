a6fd69d872c022cb1e70f39ba6b023c8ee15eeb6
Sharath C Nair <66991631+sharathcnairoffical@users.noreply.github.com>
2021-04-29 20:21:32 +0530

    Add files via upload

---
+++ config/db.config.js
@@ -0,0 +1,16 @@
+'user strict';
+
+const mysql = require('mysql');
+
+//local mysql db connection
+const dbConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Sha2752011',
+  database : 'node_mysql_crud_db'
+});
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+module.exports = dbConn;
